d9bb57d4cbfdf8ca62ecad6002d9c0d4f0760db9
Surag Puliyathamil <39499297+suragnoreply@users.noreply.github.com>
2020-05-09 13:51:54 -0700

    Add files via upload

---
+++ Run_me.m
@@ -0,0 +1,10 @@
+open_system('Sample_audio_output.slx');
+sim('Sample_audio_output.slx');
+pause(2);
+open_system('Rayleigh_Fading_Channel');
+sim('Rayleigh_Fading_Channel');
+pause(2);
+open_system('Rician_fading_channel');
+sim('Rician_fading_channel');
+
+
